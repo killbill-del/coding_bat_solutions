16b49f59cd22b94e0494d4c4e3a19fc2d5abffbc
Gopal Srivastava <71832523+killbill-del@users.noreply.github.com>
2020-11-28 16:28:25 +0530

    Update list2.py

--- list2.py
+++ list2.py
@@ -1,3 +1,4 @@
+# Question1 
 '''
 Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
 
@@ -15,6 +16,8 @@ def count_evens(nums):
       continue
   return(counts)
 
+
+# Question 2
 '''Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
 
 
@@ -26,6 +29,8 @@ def big_diff(nums):
   diff = max(nums) - min(nums)
   return(diff)
 
+
+# Question 3
 '''Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
 
 
@@ -43,6 +48,8 @@ def centered_average(nums):
     sum1 += x
   return (sum - max1 - min1) / (len(nums) - 2)
 
+
+# Question 4
 '''Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
 
 
