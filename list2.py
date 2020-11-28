fecbf2996340d907e1997721c13c1b89825ee067
Gopal Srivastava <71832523+killbill-del@users.noreply.github.com>
2020-11-28 15:48:57 +0530

    Create list2.py

---
+++ list2.py
@@ -0,0 +1,16 @@
+'''
+Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+
+count_evens([2, 1, 2, 3, 4]) → 3
+count_evens([2, 2, 0]) → 3
+count_evens([1, 3, 5]) → 0
+'''
+def count_evens(nums):
+  counts = 0
+  for i in range(len(nums)):
+    if nums[i] %2 == 0:
+      counts += 1
+    else:
+      continue
+  return(counts)
